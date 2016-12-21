d7d0b952382527893353ab7ba58b823e1e9c2f08
Henrique dos S Goulart <henriquedsg89@gmail.com>
2016-12-20 23:55:06 -0200

    Finished exercise 1
    Using the combinations formula to find pascal value for a given row and
    col.
    
    Also avoiding factorial for negative number and division by zero errors

--- course1/week1/recfun/src/main/scala/recfun/Main.scala
+++ course1/week1/recfun/src/main/scala/recfun/Main.scala
@@ -1,5 +1,7 @@
 package recfun
 
+import scala.util.Try
+
 object Main {
   def main(args: Array[String]) {
     println("Pascal's Triangle")
@@ -11,17 +13,28 @@ object Main {
   }
 
   /**
-   * Exercise 1
-   */
-    def pascal(c: Int, r: Int): Int = ???
-  
-  /**
-   * Exercise 2
-   */
-    def balance(chars: List[Char]): Boolean = ???
-  
-  /**
-   * Exercise 3
-   */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    * Exercise 1
+    */
+  def pascal(col: Int, row: Int): Int = {
+    val divisor = factorial(col) * Try(factorial(row - col)).getOrElse(1)
+    val dividend = factorial(row)
+    Try(dividend / divisor).getOrElse(1)
   }
+
+  /**
+    * Exercise 2
+    */
+  def balance(chars: List[Char]): Boolean = ???
+
+  /**
+    * Exercise 3
+    */
+  def countChange(money: Int, coins: List[Int]): Int = ???
+
+  private def factorial(n: Int): Int = {
+    if (n < 0) throw new ArithmeticException(s"There is no factorial for negative number: $n")
+    else if (n == 0) 1
+    else n * factorial(n - 1)
+  }
+}
+
