7515037d49ca8fab5a30e4d758c35d2e88568483
Henrique dos S Goulart <henriquedsg89@gmail.com>
2016-12-21 20:49:39 -0200

    Finished exercise 3
    Take a good time to think about second recursive part `+
    countChange(money, coins)`.
    
    -- i wrote a test and draw the recursive flow in a paper =(

--- course1/week1/recfun/src/main/scala/recfun/Main.scala
+++ course1/week1/recfun/src/main/scala/recfun/Main.scala
@@ -18,30 +18,30 @@ object Main {
   def pascal(col: Int, row: Int): Int = {
     val divisor = factorial(col) * Try(factorial(row - col)).getOrElse(1)
     val dividend = factorial(row)
-    Try(dividend / divisor).getOrElse(1)
+    Try(dividend / divisor).getOrElse(1) // avoid weird coursera '/ by zero'
   }
 
   /**
     * Exercise 2
     */
   def balance(chars: List[Char]): Boolean = {
-    if (chars.isEmpty) true
-    else {
-      def check(chars: List[Char], level: Int): Int = {
-        if (chars.isEmpty || level < 0) level // abort recursion if level is negative
-        else if (chars.head == '(') check(chars.tail, level + 1)
-        else if (chars.head == ')') check(chars.tail, level - 1)
-        else check(chars.tail, level)
-      }
-      val levelReached = check(chars, 0)
-      levelReached == 0
+    def validate(chars: List[Char], level: Int): Int = {
+      if (chars.isEmpty || level < 0) level // abort recursion if level is negative
+      else if (chars.head == '(') validate(chars.tail, level + 1)
+      else if (chars.head == ')') validate(chars.tail, level - 1)
+      else validate(chars.tail, level)
     }
+    validate(chars, 0) == 0
   }
 
   /**
     * Exercise 3
     */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (money == 0) 1
+    else if (money < 0 || coins.isEmpty) 0
+    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
+  }
 
   private def factorial(n: Int): Int = {
     if (n < 0) throw new ArithmeticException(s"There is no factorial for negative number: $n")
@@ -49,4 +49,3 @@ object Main {
     else n * factorial(n - 1)
   }
 }
-
